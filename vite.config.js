0fe66c088fa4bee08f9dc657eef9e453948bb68b
celest6667 <celestin.petit@my-digital-school.org>
2025-10-19 14:34:50 +0200

    Finalisation de la section films et de la grille + création des pages films

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
@@ -7,9 +6,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        inception: resolve(__dirname, "films/inception.html"),
-        matrix: resolve(__dirname, "films/matrix.html"),
-        // etc.
+        interstellar: resolve(__dirname, "films/interstellar.html"),
+        HarryPotteretlaChambredesSecrets: resolve(__dirname, "films/Harry Potter et la Chambre des Secrets.html"),
+        Superman: resolve(__dirname, "films/Superman.html"),
+        LeParrain: resolve(__dirname, "films/Le Parrain.html"),
+        Conjuring: resolve(__dirname, "films/Conjuring : L'Heure du Jugement.html"),
+        ThedarkKnight: resolve(__dirname, "films/The Dark Knight : Le chevalier noir.html"),
+        FightClub: resolve(__dirname, "films/Fight Club.html"),
+        Lesevades: resolve(__dirname, "films/Les Évadés.html"),
       }
     }
   }
